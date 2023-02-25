3eb2a055974156e872de2e08ac1e420f9f237b0a
Luis 🦎 <luis.6496@hotmail.com>
2023-02-25 12:04:27 -0600

    Add function to simulate that we're sending an email'

--- lib/sender.ex
+++ lib/sender.ex
@@ -3,16 +3,9 @@ defmodule Sender do
   Documentation for `Sender`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Sender.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def send_email(email) do
+    Process.sleep(3000)
+    IO.puts("Email to #{email} sent")
+    {:ok, "email_sent"}
   end
 end
